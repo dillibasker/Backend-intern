0dacdfc982d476a275562a21aac06ab0c712c882
Dilli Basker <dillibasker1@gmail.com>
2025-05-02 19:36:06 +0530

    fc

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://frontend-intern-9dr4.onrender.com/',  // Allow only your frontend URL
+  methods: ['GET', 'POST'],
+}));
 app.use(bodyParser.json());
 
 // MongoDB Connection
