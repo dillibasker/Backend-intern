568efc972e0c30102788a1a15035f8d434799375
Dilli Basker <dillibasker1@gmail.com>
2025-05-04 08:30:48 +0530

    fc

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://frontend-intern-taupe.vercel.app/',  // Allow only your frontend URL
+  origin: 'https://frontend-intern-taupe.vercel.app',  // Allow only your frontend URL
   methods: ['GET', 'POST'],
 }));
 app.use(bodyParser.json());
